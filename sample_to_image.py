e2a3a43bc199768f07c3da5c06a4c4bc2b3cf5b8
MaxSloofAca <100281288+MaxSloofAca@users.noreply.github.com>
2022-02-25 14:23:43 +0100

    Create sample_to_image.py

---
+++ sample_to_image.py
@@ -0,0 +1,54 @@
+
+import numpy as np
+from math import sqrt, ceil
+import cv2
+import os
+
+dir = "/media/osboxes/thesis_benign/benign-net" # Directory of samples
+os.chdir(dir)
+directory = os.fsencode(dir)
+
+for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+    if filename.endswith(".exe"):
+        # Read the whole file to data
+        with open(filename, 'rb') as binary_file:
+            data = binary_file.read()
+
+        # bin_len = 480*480
+        # data = data[:bin_len]
+        # print(data)
+
+        # Data length in bytes
+        data_len = len(data)
+
+        # d is a vector of data_len bytes
+        d = np.frombuffer(data, dtype=np.uint8)
+
+        # Assume image shape should be close to square
+        sqrt_len = int(ceil(sqrt(data_len)))  # Compute square toot and round up
+
+        # Requiered length in bytes.
+        new_len = sqrt_len * sqrt_len
+
+        # Number of bytes to pad (need to add zeros to the end of d)
+        pad_len = new_len - data_len
+
+        # Pad d with zeros at the end.
+        # padded_d = np.pad(d, (0, pad_len))
+        padded_d = np.hstack((d, np.zeros(pad_len, np.uint8)))
+
+        # Reshape 1D array into 2D array with sqrt_len pad_len x sqrt_len (im is going to be a Grayscale image).
+        im = np.reshape(padded_d, (sqrt_len, sqrt_len))
+
+        # Save image
+        imagename= os.path.splitext(file)[0]
+        cv2.imwrite(f"/media/osboxes/thesis_benign/Images/{imagename}.png", im)
+
+        
+        # Display image
+        # cv2.imshow('im' ,im)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+        continue
+
