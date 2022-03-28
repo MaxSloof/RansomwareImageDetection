9c28b982a49e04e17da8702af63b17ca985319b8
MaxSloofAca <100281288+MaxSloofAca@users.noreply.github.com>
2022-03-28 17:03:13 +0200

    New versions

--- misc/kaggle_local_data_transfer_win.py
+++ misc/kaggle_local_data_transfer_win.py
@@ -1,10 +1,23 @@
 import os
 import kaggle
-from plyer import notification
 import time
 
 # Command for Terminal - Copy underlying line #
+# Windows line:
 # python /Users/Max/Documents/GitHub/misc/kaggle_local_data_transfer_win.py
+
+# MacOS line:
+# python3 /Users/maxsloof/Github/data_acq/misc/kaggle_local_data_transfer_mac.py
+
+# Ask whether you are on MacOS or Windows
+print("\n")
+print("What Operating System are you using?")
+print("Windows (0) / MacOS (1)")
+userchoiceOS = int(input("Enter number: "))
+print("--------------")
+
+if userchoiceOS == 0:
+    from plyer import notification
 
 # Ask what notebook you want to save the latest version of
 print("What notebook do you want to save the latest version of?")
@@ -12,6 +25,7 @@ print("CNN (0) / DCGAN (1) / DCGAN-Classification (2) / ResNet (3) / DenseNet (4
 userchoice = int(input("Enter number: "))
 print("--------------")
 
+# Determine the type of NN
 if userchoice == 0:
     nt_type = "cnn-kaggle"
     type_dir = "cnn"
@@ -37,37 +51,47 @@ elif userchoice == 4:
     type_dir = "DenseNet"
     search_file = "DenseNet"
 
+# Retrieve the notebook status from Kaggle
 status = str(kaggle.api.kernel_status(user_name="maxsloof", kernel_slug=nt_type))
 
+# Show progress in CMD / Terminal
 while "queued" in status:
-    print("Notebook still queued")
+    print(f"{nt_type} notebook still queued")
     time.sleep(30)
     status = str(kaggle.api.kernel_status(user_name="maxsloof", kernel_slug= nt_type))
     
 while "running" in status:
-    print("Notebook still running")
+    print(f"{nt_type} notebook still running")
     time.sleep(30)
     status = str(kaggle.api.kernel_status(user_name="maxsloof", kernel_slug= nt_type))
 
+# Show notification when notebook is complete / has encountered error
 if "complete" in status:
-    notification.notify(
-        title=f"{nt_type} notebook has finished",
-        message=f"The {nt_type} notebook has been completed on Kaggle. The output will now be saved to the a new folder in the GitHub directory.",
-        timeout = 30
-    )
+    if userchoiceOS == 0:
+        notification.notify(
+            title=f"{nt_type} notebook has finished",
+            message=f"The {nt_type} notebook has been completed on Kaggle. The output will now be saved to the a new folder in the GitHub directory.",
+            timeout = 30
+        )
+    if userchoiceOS == 1:
+        print("Notebook has been completed!")
 
 if "error" in status:
-    notification.notify(
-        title=f"{nt_type} notebook encountered error",
-        message=f"The {nt_type} notebook has encountered an error on Kaggle. The output will now be saved to the a new folder in the GitHub directory.",
-        timeout = 30
-    )
+    if userchoiceOS == 0:
+        notification.notify(
+            title=f"{nt_type} notebook encountered error",
+            message=f"The {nt_type} notebook has encountered an error on Kaggle. The output will now be saved to the a new folder in the GitHub directory.",
+            timeout = 30
+        )
+    if userchoiceOS == 1:
+        print("Error has been encountered!")
 
+# Set right directory based on OS
+if userchoiceOS == 0:
+    dir = f"/Users/Max/Documents/GitHub/{type_dir}"
+if userchoiceOS == 1:
+    dir = f"/Users/maxsloof/Github/data_acq/{type_dir}"
 
-
-
-
-dir = f"/Users/Max/Documents/GitHub/{type_dir}"
 vnum = 1
 # Find the latest version number
 file_exists = []
@@ -98,7 +122,11 @@ print(f"New folder name: {nt_type}-v{new_vnum:03}")
 print("--------------")
 
 # Create new folder with the name of the notebook and the version number
-new_dir = f"/Users/Max/Documents/Github/{type_dir}/{nt_type}-v{new_vnum:03}"
+if userchoiceOS == 0:
+    new_dir = f"/Users/Max/Documents/Github/{type_dir}/{nt_type}-v{new_vnum:03}"
+if userchoiceOS == 1:
+    new_dir = f"/Users/maxsloof/Github/data_acq/{type_dir}/{nt_type}-v{new_vnum:03}"
+
 os.mkdir(new_dir)
 
 # Download checkpoints from latest run
